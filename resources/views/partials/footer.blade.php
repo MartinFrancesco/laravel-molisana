dbbc30dd6cf00a9b8cb579836a0e3a071d1efcdf
Francesco <francescomartinmail@gmail.com>
2020-12-22 19:31:46 +0100

    laravel-molisana

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <strong>
+        &copy; 2020
+    </strong>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <a href="/" class="brand">
+    <img src="{{ asset('img/logo.png')}}" alt="">
+    </a>
+</header>
